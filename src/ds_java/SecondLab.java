1bd08e99957ef8c1e09261d12010d84548da29c3
wch960426@126.com <wch960426@126.com>
2017-04-08 20:28:48 +0800

    Lab2：中缀表达式转后缀表达式

---
+++ src/ds_java/SecondLab.java
@@ -0,0 +1,108 @@
+package ds_java;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class SecondLab {
+	public static void main(String[] args){
+		System.out.println("输入一个中缀表达式，如果需要退出请输入exit");
+		Scanner scanner = new Scanner(System.in);
+		String input = "";
+		input = scanner.nextLine();
+		while(input.length()!= 0 && !"exit".equalsIgnoreCase(input)){
+			System.out.println("中缀表达式：");
+			System.out.println(input);
+			System.out.println("后缀表达式：");
+			Reverse(input);
+			System.out.println("请再输入一个表达式，exit退出");
+			input = scanner.nextLine();
+		}
+		System.out.println("program exit");
+	}
+	
+	/*将中缀表达式改写成后缀表达式*/
+	private static void Reverse(String input) throws IllegalArgumentException, NumberFormatException{
+		int len = input.length();
+		char c,tempchar;
+		Stack<Character> s1 = new Stack<Character>();//s1存放操作码
+		Stack<Double> s2 = new Stack<Double>();//s2存放操作数
+		double number;
+		int lastIndex = -1;
+		for(int i=0; i<len; ++i){
+			c = input.charAt(i);
+			if (Character.isDigit(c) || c == '.'){
+				lastIndex = readDouble(input, i);
+				number = Double.parseDouble(input.substring(i, lastIndex));//把字符串的数字转化为double类型
+				s2.push(number);//遇到数字就进栈
+				i = lastIndex - 1;
+				
+				
+			}
+		}
+	}
+	
+	/*计算结果*/
+	private static double calc(double num1, double num2, char op){
+		switch(op){
+		case '+' :
+			return num1 + num2;
+		case '-' :
+			return num1 - num2;
+		case '*':
+			return num1 * num2;
+		case '/':
+			if (num2 == 0)
+				throw new IllegalArgumentException("被除数不能为0");
+			return num1 / num2;
+		default:
+				return 0;
+		}
+	}
+	
+	/*判断两个操作码的优先级
+	 * 平级返回0
+	 * op1大于op2返回1
+	 * op1小于op2返回0
+	 * */
+	private static int priortyCompare(char op1, char op2){
+		switch(op1){
+		case '+':
+		case '-':
+			return (op2 == '*' || op2 == '/' ? -1 : 0);
+		case '*':
+		case '/':
+			return (op2 == '+' || op2 == '-' ? 1 :0);
+		}
+		return 1;
+	}
+	
+	/*判断是否操作码*/
+	private static boolean isOperator(char c){
+		return( c == '+' || c == '-' || c == '*' || c == '/');
+	}
+	
+	//读取哪几位是数字，返回的是数字结束的位置
+	private static int readDouble(String input, int start) throws IllegalArgumentException{
+		int len = input.length();
+		int dotIndex = -1;
+		char c;
+		for (int i = start; i<len; ++i){
+			c = input.charAt(i);
+			//判断小数点
+			if(c == '.'){
+				if(dotIndex != -1)
+					throw new IllegalArgumentException("表达式错误，表达式中只可以有一个小数点");
+				else if ( i == len-1)
+					throw new IllegalArgumentException("小数点不能在最后一位");
+				else dotIndex = i;
+			}else if (!Character.isDigit(c)){
+				if(dotIndex == -1 || i - dotIndex > 1)
+					return i;
+				else throw new IllegalArgumentException("小数点的位置错误");
+			}else if (i == len - 1){
+				return len;
+			}
+		}
+		throw new IllegalArgumentException("输入的不是一个数字");
+	}
+}
