36e683a49c0f148ba4c606e992eb299848296ada
wch960426@126.com <wch960426@126.com>
2017-04-08 15:42:05 +0800

    注释添加

--- src/ds_java/FirstLab.java
+++ src/ds_java/FirstLab.java
@@ -11,7 +11,7 @@ import ds_java.Onelink;
 public class FirstLab {
 	
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)  {
 		
 		String go;
 		Onelink list;
@@ -25,7 +25,7 @@ public class FirstLab {
 			System.out.println("请输入相邻的位数");
 			local = (int)br.nextInt();
 			mylab.Adjmax(list, local);
-			System.out.println("是否继续 : Yes or No?");			
+			System.out.println("继续请输入：Yes");			
 			go =  br.next();
 		}while(go.equals("Yes") );
 		
@@ -49,12 +49,13 @@ public class FirstLab {
 		return list;
 	}
 	
+	 /*求相邻最大和*/
 	 void Adjmax(Onelink L,int k ){
 		OnelinkNode p1,p2;
 		int k1;
 		int sum=0,max=0;
-		int location=0;
-		int val=0;
+		int location=0;//记录最大值位置
+		int val=0;//记录最大的data
 		p1 = L.head;
 		if(p1 == null || (L.length())-1 < k ){
 			 System.out.println("输入有误");
